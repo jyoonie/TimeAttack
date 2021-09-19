a3dd8713efc6773e17c2b460edb8f02d8dec3a6f
jyoonie <jywoo92324@gmail.com>
2021-09-19 22:23:37 +0900

    추가: 주식 정보 크롤링 직전까지 완료

--- app.py
+++ app.py
@@ -2,21 +2,28 @@ from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
+import requests
+from bs4 import BeautifulSoup
+
 from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
 db = client.dbStock
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
 @app.route('/code', methods=['GET'])
 def selection():
+    db.user.drop()
     # indicators = db.codes.distinct('group')
     indicators = list(db.codes.find({'group': 'market'}, {'_id': False}))
 
     return jsonify({'all_items': indicators})
+
 
 @app.route('/code2', methods=['GET'])
 def selection2():
@@ -25,12 +32,14 @@ def selection2():
 
     return jsonify({'all_items': indicators})
 
+
 @app.route('/code3', methods=['GET'])
 def selection3():
     # indicators = db.codes.distinct('group')
     indicators = list(db.codes.find({'group': 'tag'}, {'_id': False}))
 
     return jsonify({'all_items': indicators})
+
 
 @app.route('/codes', methods=['POST'])
 def save_info():
@@ -44,6 +53,7 @@ def save_info():
 
     return jsonify({'code1': item})
 
+
 @app.route('/codes2', methods=['POST'])
 def save_info2():
     name_receive = request.form['name_give']
@@ -55,6 +65,7 @@ def save_info2():
     db.user.insert_one(doc)
 
     return jsonify({'code2': item})
+
 
 @app.route('/codes3', methods=['POST'])
 def save_info3():
@@ -68,7 +79,8 @@ def save_info3():
 
     return jsonify({'code3': item})
 
-@app.route('/stock', methods = ['GET'])
+
+@app.route('/stock', methods=['GET'])
 def save_info4():
     users = list(db.user.find({}, {'_id': False}))
 
@@ -79,6 +91,32 @@ def save_info4():
     rec = list(db.stocks.find({'market': code1, 'sector': code2, 'tag': code3}, {'_id': False}))
 
     return jsonify({'user': rec})
+
+
+@app.route('/stock2', methods=['POST'])
+def show_info():
+    code_receive = request.form['code_give']
+
+    url = 'https://finance.naver.com/item/main.nhn?code=' + code_receive
+
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+
+    data = requests.get(url, headers=headers)
+
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    # chart_area > div.rate_info > div > p.no_today
+    # tab_con1 > div.first > table > tbody > tr.strong > td
+    # _per
+
+    주가 = soup.select_one('#chart_area > div.rate_info > div > p.no_today')
+    시총 = soup.select_one('#tab_con1 > div.first > table > tbody > tr.strong > td')
+    PER = soup.select_one('#_per')
+
+    doc = {'주가': 주가, '시총': 시총, 'PER': PER}
+    db.info.insert_one(doc)
+
+    return jsonify({'msg': 'success!'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
--- test.py
+++ test.py
@@ -1,18 +1,25 @@
-from pymongo import MongoClient
+import requests
+from bs4 import BeautifulSoup
 
-client = MongoClient('localhost', 27017)
-db = client.dbStock
+def show_info():
+    code_receive = '035720'
 
-indicators = list(db.codes.find({'group': 'market'}, {'_id': False}))
+    url = 'https://finance.naver.com/item/main.nhn?code=' + code_receive
 
-print(indicators[0]['name'])
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 
-users = list(db.user.find({}, {'_id': False}))
+    data = requests.get(url, headers=headers)
 
-print(users)
+    soup = BeautifulSoup(data.text, 'html.parser')
 
-code1 = users[0]['codes1']
-code2 = users[1]['codes2']
-code3 = users[2]['codes3']
+    # chart_area > div.rate_info > div > p.no_today
+    # chart_area > div.rate_info > div > p.no_today
 
-rec = list(db.stocks.find({'market': code1, 'sector': code2, 'tag': code3}, {'_id': False}))
+    # tab_con1 > div.first > table > tbody > tr.strong > td
+    # _per
+
+    주가 = soup.select_one('#chart_area > div.rate_info > div > p.no_today')
+    시총 = soup.select_one('#tab_con1 > div.first > table > tbody > tr.strong > td')
+    PER = soup.select_one('#_per')
+
+    print(주가, 시총, PER)
