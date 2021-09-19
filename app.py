83cc18a28675b6923cc43371055003f1bdbf156f
jyoonie <jywoo92324@gmail.com>
2021-09-19 18:30:30 +0900

    추가: 정보 탭 스크래핑 전까지 구현

--- app.py
+++ app.py
@@ -18,16 +18,67 @@ def selection():
 
     return jsonify({'all_items': indicators})
 
-@app.route('/stock', methods=['POST'])
-def save_info():
-    info = request.json
-    return jsonify(stocks)
+@app.route('/code2', methods=['GET'])
+def selection2():
+    # indicators = db.codes.distinct('group')
+    indicators = list(db.codes.find({'group': 'sector'}, {'_id': False}))
 
-@app.route('/test', methods=['POST'])
-def test_post():
-    title_receive = request.form['title_give']
-    print(title_receive)
-    return jsonify({'result': 'success', 'msg': '이 요청은 POST!'})
+    return jsonify({'all_items': indicators})
+
+@app.route('/code3', methods=['GET'])
+def selection3():
+    # indicators = db.codes.distinct('group')
+    indicators = list(db.codes.find({'group': 'tag'}, {'_id': False}))
+
+    return jsonify({'all_items': indicators})
+
+@app.route('/codes', methods=['POST'])
+def save_info():
+    name_receive = request.form['name_give']
+    item = db.codes.find_one({'name': name_receive}, {'_id': False})['code']
+    doc = {
+        'code1': item
+    }
+
+    db.user.insert_one(doc)
+
+    return jsonify({'code1': item})
+
+@app.route('/codes2', methods=['POST'])
+def save_info2():
+    name_receive = request.form['name_give']
+    item = db.codes.find_one({'name': name_receive}, {'_id': False})['code']
+    doc = {
+        'code2': item
+    }
+
+    db.user.insert_one(doc)
+
+    return jsonify({'code2': item})
+
+@app.route('/codes3', methods=['POST'])
+def save_info3():
+    name_receive = request.form['name_give']
+    item = db.codes.find_one({'name': name_receive}, {'_id': False})['code']
+    doc = {
+        'code3': item
+    }
+
+    db.user.insert_one(doc)
+
+    return jsonify({'code3': item})
+
+@app.route('/stock', methods = ['GET'])
+def save_info4():
+    users = list(db.user.find({}, {'_id': False}))
+
+    code1 = users[0]['code1']
+    code2 = users[1]['code2']
+    code3 = users[2]['code3']
+
+    rec = list(db.stocks.find({'market': code1, 'sector': code2, 'tag': code3}, {'_id': False}))
+
+    return jsonify({'user': rec})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
--- test.py
+++ test.py
@@ -6,3 +6,13 @@ db = client.dbStock
 indicators = list(db.codes.find({'group': 'market'}, {'_id': False}))
 
 print(indicators[0]['name'])
+
+users = list(db.user.find({}, {'_id': False}))
+
+print(users)
+
+code1 = users[0]['codes1']
+code2 = users[1]['codes2']
+code3 = users[2]['codes3']
+
+rec = list(db.stocks.find({'market': code1, 'sector': code2, 'tag': code3}, {'_id': False}))
